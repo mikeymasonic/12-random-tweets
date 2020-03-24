8f0352c888881dbdc84a1e1935b85a8fc61710eb
Michael Romay <amerror@gmail.com>
2020-03-24 14:49:23 -0700

    post tweet test

--- __tests__/tweets.test.js
+++ __tests__/tweets.test.js
@@ -4,6 +4,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Tweet = require('../lib/models/Tweet');
+
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +18,19 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('posts a tweet', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({ handle: 'Mikey', text: 'What this do?' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: 'Mikey', 
+          text: 'What this do?',
+          __v: 0
+        });
+      });
   });
 });
