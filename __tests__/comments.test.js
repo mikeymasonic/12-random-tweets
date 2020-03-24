8d4311fb4ba3efee5811b2006c1ab252e8a5213b
Michael Romay <amerror@gmail.com>
2020-03-24 16:55:19 -0700

    finished comment tests

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -39,4 +39,85 @@ describe('app routes', () => {
         });
       });
   });
+
+  it('gets a comment by id', () => {
+    return Tweet.create({ 
+      handle: 'Mikey', 
+      text: 'What this do?' 
+    })
+      .then(tweet => {
+        return Comment.create({
+          comment: 'idk',
+          tweet: tweet._id
+        })
+          .then(comment => {
+            return request(app)
+              .get(`/api/v1/comments/${comment.id}`);
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              _id: expect.any(String),
+              comment: 'idk',
+              tweet: {
+                _id: expect.any(String),
+                handle: 'Mikey',
+                text: 'What this do?',
+                __v: 0
+              },
+              __v: 0
+            });
+          });
+      });
+  });
+
+  it('updates a comment by id', () => {
+    return Tweet.create({ 
+      handle: 'Mikey', 
+      text: 'What this do?' 
+    })
+      .then(tweet => {
+        return Comment.create({
+          comment: 'idk',
+          tweet: tweet._id
+        })
+          .then(commentToUpdate => {
+            return request(app)
+              .patch(`/api/v1/comments/${commentToUpdate.id}`)
+              .send({ comment: 'idk what?' });
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              _id: expect.any(String),
+              comment: 'idk what?',
+              tweet: expect.any(String),
+              __v: 0
+            });
+          });
+      });
+  });
+
+  it('deletes a comment by id', () => {
+    return Tweet.create({ 
+      handle: 'Mikey', 
+      text: 'What this do?' 
+    })
+      .then(tweet => {
+        return Comment.create({
+          comment: 'idk',
+          tweet: tweet._id
+        })
+          .then(commentToDelete => {
+            return request(app)
+              .delete(`/api/v1/comments/${commentToDelete.id}`);
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              _id: expect.any(String),
+              comment: 'idk',
+              tweet: expect.any(String),
+              __v: 0
+            });
+          });
+      });
+  });
 });
