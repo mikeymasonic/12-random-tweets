b9a497acd33afe71f6ecb3a783daa79ca9879487
Michael Romay <amerror@gmail.com>
2020-03-25 09:01:41 -0700

    added more things

--- __tests__/tweets.test.js
+++ __tests__/tweets.test.js
@@ -5,6 +5,7 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Tweet = require('../lib/models/Tweet');
+const Comment = require('../lib/models/Comment');
 
 
 describe('routes', () => {
@@ -64,14 +65,47 @@ describe('routes', () => {
       text: 'What this do?'
     })
       .then(tweet => {
+        return Comment.create({
+          comment: 'idk',
+          tweet: tweet._id
+        });
+      })
+      .then(tweet => {
         return request(app)
-          .get(`/api/v1/tweets/${tweet.id}`);
+          .get(`/api/v1/tweets/${tweet.tweet}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual(
+          [{
+            _id: expect.any(String),
+            handle: 'Mikey',
+            text: 'What this do?',
+            __v: 0
+          }, [{
+            _id: expect.any(String),
+            comment: 'idk',
+            tweet: expect.any(String),
+            __v: 0
+          }]]
+        );
+      });
+  });
+
+  it('updates a tweets text', () => {
+    return Tweet.create({
+      handle: 'Mikey',
+      text: 'What this do?'
+    })
+      .then(tweet => {
+        return request(app)
+          .patch(`/api/v1/tweets/${tweet.id}`)
+          .send({ text: 'Are you there?' });
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           handle: 'Mikey',
-          text: 'What this do?',
+          text: 'Are you there?',
           __v: 0
         });
       });
